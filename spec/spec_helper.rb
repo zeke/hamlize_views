61237444164e16ffec8a9cdbb1bee1ea2632e638
Zeke Sikelianos <zeke@sikelianos.com>
2009-03-30 21:47:49 -0700

    Initial commit to hamlize_views.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'hamlize_views'
+
+Spec::Runner.configure do |config|
+  
+end
